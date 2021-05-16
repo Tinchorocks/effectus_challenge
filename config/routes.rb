3d4247737b52f0136ed49547ccab3038e6d83dc6
Martin Mariño <martinmb96@gmail.com>
2021-05-15 23:45:04 -0300

    Add endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get '/transactions', to: 'transactions#index'
+  post '/transfer', to: 'transactions#transfer'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
 end
